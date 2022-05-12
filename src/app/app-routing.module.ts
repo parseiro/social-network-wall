2f80d92b7deff0b64ffbdaf1647914afb2cee102
Leonardo Vilela Pinheiro <leonardo@vilelapinheiro.com>
2022-05-12 10:29:10 -0300

    Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./pages/login/login.component";
+import {CreateAccountComponent} from "./pages/create-account/create-account.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: 'create-account', component: CreateAccountComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
